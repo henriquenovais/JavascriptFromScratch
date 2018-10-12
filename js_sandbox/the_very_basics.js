7dd88442b53e8f6d5c6625c608420b53dc68f6b7
Henrique Novais <henriquennoliveira@gmail.com>
2018-10-12 00:26:18 -0300

    Add commenting on the usage of the Math Object

--- js_sandbox/the_very_basics.js
+++ js_sandbox/the_very_basics.js
@@ -35,9 +35,10 @@
 //CONST
 //Constants can be changed but cannot be reassigned.
 
-const numbers = [1,2,3,4,5];
-numbers.push(6); //Include number 6
-console.log(numbers);
+// const numbers = [1,2,3,4,5];
+// numbers.push(6); //Include number 6
+// console.log(numbers);
+// console.log(typeof numbers)
 
 // numbers = [1,2,3]; // can't do that, numbers is a constant!
 
@@ -60,10 +61,36 @@ Dates
 Anything else...
 */
 
+//NaN is a value, means "not a number" and is used in case 
+//the programmer tries to convert a certain type into a number 
+//such as a string.
+
 //JavaScript is dynamically typed but there are supersets that may 
 //allow static typing such as Flow and Typescript
 
 //To get an undefined variable error the undefined variable must
 //have been declared as a constant! 
+
+//Math Object
+// let val;
+
+// val = Math.PI;
+// val = Math.E;
+// val = Math.round(2.4);
+// val = Math.ceil(2.4);
+// val = Math.floor(2.8);
+// val = Math.sqrt(64);
+// val = Math.abs(-3);
+// val = Math.pow(8,2);
+// val = Math.random();
+
+// console.log(val);
+
+
+
+
+
+
+
 
 
