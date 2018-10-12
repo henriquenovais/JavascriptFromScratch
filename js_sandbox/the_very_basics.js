f4ae36888804087c375fca33bc950969f90d86a5
Henrique Novais <henriquennoliveira@gmail.com>
2018-10-11 23:57:41 -0300

    Added commenting based on Reference Types and Primitive types

---
+++ js_sandbox/the_very_basics.js
@@ -0,0 +1,69 @@
+// console.log("hello world!");
+// console.log(123);
+// console.log(true);
+// var greeting = 'Hello!';
+// console.log([1,2,3,4]);
+// console.log({a: "this is an object", b: greeting});
+// console.table({a: "this is an object", b: greeting});
+
+// console.error('This is some error!');
+// // console.clear(); // to clear the console
+// console.warn('This is a warning!');
+// console.time('Hello!')
+//     console.log("hello world!");
+//     console.log("hello world!");
+//     console.log("hello world!");
+//     console.log("hello world!");
+//     console.log("hello world!");
+//     console.log("hello world!");
+// console.timeEnd('Hello!'); //Counts the time to execute this functions!
+
+//To open console press F12 on Chrome
+//The console can be used as an input to help debug the code
+
+// //LET
+// let name;
+// name = 'John';
+// console.log(name);
+
+// //VAR
+
+// var otherName;
+// otherName = "Kike";
+// console.log(otherName);
+
+//CONST
+//Constants can be changed but cannot be reassigned.
+
+const numbers = [1,2,3,4,5];
+numbers.push(6); //Include number 6
+console.log(numbers);
+
+// numbers = [1,2,3]; // can't do that, numbers is a constant!
+
+//PRIMITIVE DATA TYPES
+/*
+String
+Numbers 
+Boolean
+Null
+Undefined
+Symbols(ES6)
+*/
+
+//REFERENCE TYPES - Objects
+/*
+Arrays
+Object Literals
+Functions
+Dates
+Anything else...
+*/
+
+//JavaScript is dynamically typed but there are supersets that may 
+//allow static typing such as Flow and Typescript
+
+//To get an undefined variable error the undefined variable must
+//have been declared as a constant! 
+
+
